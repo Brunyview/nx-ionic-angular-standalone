572798011be0d5bfb19e7d134911aa0c9e8b6d6f
Chris Thompson <chris.thompson@brunyview.com>
2022-06-19 10:55:16 +1000

    eat: added tab2 with its own query result store

--- libs/ionic-app/tab2/feature-tab2/src/lib/tab2.page.ts
+++ libs/ionic-app/tab2/feature-tab2/src/lib/tab2.page.ts
@@ -1,7 +1,9 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
+import { QueryResultStore } from '@my-org/ionic-app/shared/product/store-query-result';
 import { ExploreContainerComponent } from '@my-org/shared/ui-explore-container';
 
 @Component({
@@ -14,10 +16,32 @@ import { ExploreContainerComponent } from '@my-org/shared/ui-explore-container';
     </ion-header>
     <ion-content [fullscreen]="true">
       <ion-header collapse="condense">
-        <ion-toolbar> <ion-title size="large">Tab 2</ion-title></ion-toolbar>
+        <ion-toolbar>
+          <ion-title size="large">Tab 2</ion-title>
+        </ion-toolbar>
       </ion-header>
-      <my-org-explore-container name="Tab 2 page"></my-org-explore-container>
-    </ion-content> `,
+      <ion-list>
+        <ion-item (click)="search1()" detail="true">
+          <ion-label>Search Queensland</ion-label>
+        </ion-item>
+        <ion-item (click)="search2()" detail="true">
+        <ion-label>Search Victoria</ion-label>
+        </ion-item>
+      </ion-list>
+    </ion-content>`,
   imports: [IonicModule, CommonModule, FormsModule, ExploreContainerComponent],
 })
-export class Tab2Page {}
+export class Tab2Page {
+  constructor(private readonly store: QueryResultStore, private readonly router: Router) { }
+
+  search1() {
+    this.store.updateQuery({ states: 'QLD' });
+    this.router.navigate(['tab2', 'products']);
+  }
+
+  search2() {
+    this.store.updateQuery({ states: 'VIC' });
+    this.router.navigate(['tab2', 'products']);
+  }
+
+}
--- libs/ionic-app/tab2/feature-tab2/src/lib/tab2.routing.ts
+++ libs/ionic-app/tab2/feature-tab2/src/lib/tab2.routing.ts
@@ -1,9 +1,26 @@
 import { Route } from '@angular/router';
-import { Tab2Page } from './tab2.page';
 
 export const routes: Route[] = [
   {
     path: '',
     loadComponent: () => import('./tab2.page').then((m) => m.Tab2Page),
+    loadChildren: () =>
+      import('@my-org/ionic-app/shared/product/feature-product-list').then(
+        (m) => m.routes
+      ),
+  },
+  {
+    path: 'products',
+    loadComponent: () =>
+      import('@my-org/ionic-app/shared/product/feature-product-list').then(
+        (m) => m.ProductListComponent
+      ),
+  },
+  {
+    path: 'products/:id',
+    loadChildren: () =>
+      import('@my-org/ionic-app/shared/product/feature-product-details').then(
+        (m) => m.routes
+      ),
   },
 ];
